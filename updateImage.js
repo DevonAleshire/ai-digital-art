62d9751d79835d0175b81249b7335536f9786506
DevonAleshire <devon.aleshire@gmail.com>
2024-12-29 16:35:46 -0800

    Add cron job for automating image refresh

--- updateImage.js
+++ updateImage.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import OpenAI from "openai";
 import axios from "axios";
+import cron from 'node-cron';
 import fs from "fs";
 import { once } from "events";
 
@@ -154,5 +155,20 @@ async function fetchImage() {
   }
 }
 
-// Run the process
-fetchImage();
+cron.schedule('*/5 * * * *', async () => {
+  console.log('Fetching a new image...\n');
+  
+  // Start Time
+  const startTime = performance.now();
+  
+  // Run the process
+  fetchImage();
+  
+  // End Time
+  const endTime = performance.now();
+  const executionTime = `Execution Time: ${(endTime - startTime).toFixed(2)}ms\n`;
+  
+  // Log to file
+  fs.appendFileSync(`execution_time.log`, executionTime, 'utf8');
+
+});
