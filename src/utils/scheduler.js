51c8fdb4509779db3d4ffba425c63b3666b6fe6d
Devon Aleshire <devon.aleshire@gmail.com>
2026-04-15 08:34:16 -0700

    Optimize util tools

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -1,19 +1,13 @@
 import cron from "node-cron";
 import imageService from "../services/imageService.js";
+import fileUtils from "./fileUtils.js";
 
-// // Schedule the script to run periodically
 cron.schedule("*/1 * * * *", async () => {
   console.log("Fetching a new image...\n");
 
-  // Start Time
   const startTime = performance.now();
-
-  // Run the process
   await imageService.fetchImage();
-
-  // End Time
   const endTime = performance.now();
 
-  writeToExecutionLog();
-  console.log(executionTime);
+  fileUtils.writeToExecutionLog(startTime, endTime);
 });
